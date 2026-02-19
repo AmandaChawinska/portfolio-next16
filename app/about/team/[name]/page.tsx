9617ce95d3157b52a2911b7814b7e9a788aaabba
Amanda Chawinska <amanda.ch@gqg.pl>
2026-02-19 10:55:11 +0100

    dark & light mode

--- app/about/team/[name]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const DynamicPages = async ({ params }) => {
-  const name = (await params).name;
-  return <div className="">DynamicPages {name}</div>;
-};
-
-export default DynamicPages;
